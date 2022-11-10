06ffba654d4e9253da821bbe9aff033dfa45a53c
konjikun <107373499+konjikun@users.noreply.github.com>
2022-11-10 17:58:24 +0900

    7-1

---
+++ cs2/jyugyou/7-1/2.py
@@ -0,0 +1,13 @@
+import math
+
+a = -(1/6)* math.log2((1/6))-(1/6)* math.log2((1/6))-(1/6)* math.log2((1/6))-(1/6)* math.log2((1/6))-(1/6)* math.log2((1/6))-(1/6)* math.log2((1/6))
+print(a)
+
+b = -(1/2)* math.log2((1/2)) -(1/12)* math.log2((1/12))-(1/12)* math.log2((1/12))-(1/12)* math.log2((1/12))-(1/12)* math.log2((1/12))-(1/12)* math.log2((1/12))
+print(b)
+
+lists =sorted([1, 3, 10, 18, 22, 27, 35, 54, 55, 68, 80])
+print(lists)
+
+lists =sorted([2, 5, 12, 24, 31, 38, 43, 48, 57, 69, 88, 93, 99])
+print(lists)
